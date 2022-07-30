ae6b49a6077395cce91fc9a13cc32ce8d9b7184a
sanath nath <sanathnadh54@gmail.com>
2022-07-30 16:04:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppRoutes from './AppRoutes';
+import Header from './Container/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
